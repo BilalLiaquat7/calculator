57693df4161a27cc958fc61030a95d1745fdc6e2
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-08-16 17:09:28 +0500

    calculate functionality added

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,37 +1,88 @@
 import './style.css';
+import { useState } from 'react';
+import calculate from '../logic/calculate';
 
 export default function Calculator() {
+  const [value, setValue] = useState({
+    total: '',
+    next: '',
+    operation: '',
+  });
+
+  const getValue = (event) => {
+    const btn = event.target.innerHTML;
+    setValue(calculate(value, btn));
+  };
+
   return (
-    <div className='mainSection'>
-      <input className='input' readOnly='readonly'/>
-      <div className='row'>
-        <button className="simpleButton">AC</button>
-        <button className="simpleButton">+/-</button>
-        <button className="simpleButton">%</button>
-        <button className="expressionButton">÷</button>
+    <div className="mainSection">
+      <div className="input">{value.total} {value.operation} {value.next}</div>
+      <div className="row">
+        <button className="simpleButton" onClick={getValue}>
+          AC
+        </button>
+        <button className="simpleButton" onClick={getValue}>
+          +/-
+        </button>
+        <button className="simpleButton" onClick={getValue}>
+          %
+        </button>
+        <button className="expressionButton" onClick={getValue}>
+          ÷
+        </button>
       </div>
-      <div className='row'>
-        <button className="simpleButton">7</button>
-        <button className="simpleButton">8</button>
-        <button className="simpleButton">9</button>
-        <button className="expressionButton">x</button>
+      <div className="row">
+        <button className="simpleButton" onClick={getValue}>
+          7
+        </button>
+        <button className="simpleButton" onClick={getValue}>
+          8
+        </button>
+        <button className="simpleButton" onClick={getValue}>
+          9
+        </button>
+        <button className="expressionButton" onClick={getValue}>
+          x
+        </button>
       </div>
-      <div className='row'>
-        <button className="simpleButton">4</button>
-        <button className="simpleButton">5</button>
-        <button className="simpleButton">6</button>
-        <button className="expressionButton">-</button>
+      <div className="row">
+        <button className="simpleButton" onClick={getValue}>
+          4
+        </button>
+        <button className="simpleButton" onClick={getValue}>
+          5
+        </button>
+        <button className="simpleButton" onClick={getValue}>
+          6
+        </button>
+        <button className="expressionButton" onClick={getValue}>
+          -
+        </button>
       </div>
-      <div className='row'>
-        <button className="simpleButton">1</button>
-        <button className="simpleButton">2</button>
-        <button className="simpleButton">3</button>
-        <button className="expressionButton">+</button>
+      <div className="row">
+        <button className="simpleButton" onClick={getValue}>
+          1
+        </button>
+        <button className="simpleButton" onClick={getValue}>
+          2
+        </button>
+        <button className="simpleButton" onClick={getValue}>
+          3
+        </button>
+        <button className="expressionButton" onClick={getValue}>
+          +
+        </button>
       </div>
-      <div className='row'>
-        <button className="zeroButton">0</button>
-        <button className="simpleButton">.</button>
-        <button className="expressionButton">=</button>
+      <div className="row">
+        <button className="zeroButton" onClick={getValue}>
+          0
+        </button>
+        <button className="simpleButton" onClick={getValue}>
+          .
+        </button>
+        <button className="expressionButton" onClick={getValue}>
+          =
+        </button>
       </div>
     </div>
   );
