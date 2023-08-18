1a004a37f7237abec06a15b9a2323acc840cd7b8
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-08-18 23:26:53 +0500

    quote functionality added

---
+++ src/components/Quote.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+export default function Quote() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    const getData = async () => {
+      fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
+        headers: {
+          'X-Api-Key': 'fDPmZpVzpJsZrqz3Waz2xQ==BEj9NXq27CsBpFNZ',
+        },
+      })
+        .then((resp) => resp.json())
+        .then((quote) => {
+          setLoading(false);
+          console.log(quote[0].quote);
+          setQuote(quote[0].quote);
+        })
+        .catch((error) => setError(error));
+    };
+    getData();
+  }, []);
+
+  if (loading) return <h2>Loading...</h2>;
+
+  if (error) return <h2>Error...</h2>;
+
+  return <h3>{quote}</h3>;
+}
