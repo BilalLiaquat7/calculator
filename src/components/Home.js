ef0dfdaf28fdfb23d4e2d9d3eec704a75379dc78
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-08-23 01:29:59 +0500

    basic functionality done

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to our page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        cupiditate, ducimus repellendus debitis asperiores quisquam, placeat
+        saepe soluta dolorem velit perspiciatis minima accusamus cum eum amet
+        corrupti eos molestiae labore! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Reiciendis cupiditate, ducimus repellendus debitis
+        asperiores quisquam, placeat saepe soluta dolorem velit perspiciatis
+        minima accusamus cum eum amet corrupti eos molestiae labore!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        cupiditate, ducimus repellendus debitis asperiores quisquam, placeat
+        saepe soluta dolorem velit perspiciatis minima accusamus cum eum amet
+        corrupti eos molestiae labore! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Reiciendis cupiditate, ducimus repellendus debitis
+        asperiores quisquam, placeat saepe soluta dolorem velit perspiciatis
+        minima accusamus cum eum amet corrupti eos molestiae labore!
+      </p>
+    </div>
+  );
+}
