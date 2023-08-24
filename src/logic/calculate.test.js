60f2d4cf304908a9bedb68bb84063330fad62c4f
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-08-24 19:22:37 +0500

    test cases added for operate and calculate functionality

---
+++ src/logic/calculate.test.js
@@ -0,0 +1,18 @@
+import calculate from './calculate.js';
+
+describe('calculate functionality', () => {
+    test('should add two numbers together and return the result in an object with a total property', () => {
+        const obj = {
+            total: '1',
+            next: '2',
+            operation: '-',
+        };
+
+        const res = calculate(obj, 'AC');
+        expect(res).toEqual({
+            total: null,
+            next: null,
+            operation: null
+        });
+    });
+})
